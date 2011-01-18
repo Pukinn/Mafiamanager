2c4fd51aa76207c91712b55a3a7f4ca08f6c5f8d
Pukinn <pukinn@hg.homeunix.org>
2011-01-18 17:27:44 +0100

    Scharping class added

---
+++ src/CharScharping.java
@@ -0,0 +1,51 @@
+/*
+	Mafiamanager - a tool to support the referee of the parlor game "Mafia"
+    Copyright (C) 2011  Thomas Högner
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import java.util.ArrayList;
+
+
+public class CharScharping {
+
+	// general
+	public String type;
+	public String name;
+	public ArrayList<Player> player;
+	public int startsize;
+	
+	
+	public CharScharping(int _startsize){
+		
+		// handover
+		startsize = _startsize;
+		
+		// initialize
+		type = "scharping";
+		player = new ArrayList<Player>();
+	}
+	
+	// returns how much players are alive
+	public int playeralive(){
+		int alive = 0;
+		
+		for (Player p : player){
+			if (p.alive) { alive++; }
+		}
+		
+		return alive;
+	}
+}
